89d5156939ea2148b916b3dd008845d0a483d44e
shivam010898 <shivamprakash862@gmail.com>
2020-09-14 12:29:51 +0530

    "Merge Conflict solution"

---
+++ Calculation/src/com/lti/add/AdditionServlet.java
@@ -0,0 +1,47 @@
+package com.lti.add;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AdditionServlet
+ */
+public class AdditionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AdditionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String str1;
+		String str2;
+		str1 = request.getParameter("first");
+		str2 = request.getParameter("second");
+		int d= Integer.parseInt(str1)+ Integer.parseInt(str2);
+		PrintWriter out = response.getWriter();
+		out.println("Sum is " + d);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
