546bbf66882920c9926d3649cd8b61bc1ac9094b
shivam010898 <shivamprakash862@gmail.com>
2020-09-14 11:59:37 +0530

    "to 854/java commit"

---
+++ Second/src/LoginServlet.java
@@ -0,0 +1,30 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String str;
+		str = request.getParameter("uname");
+		PrintWriter out = response.getWriter();
+		out.println("Welcome " + str);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
