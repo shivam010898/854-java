3d5e478b217614edc3a93dc9fd8c2c0cb5d645ef
shivam010898 <shivamprakash862@gmail.com>
2020-09-14 12:36:08 +0530

    "merge resolved"

--- Second/src/LoginServlet.java
+++ Second/src/LoginServlet.java
@@ -25,6 +25,7 @@ public class LoginServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
+		System.out.println("Merge Conflict Resolved");
 	}
 
 }
